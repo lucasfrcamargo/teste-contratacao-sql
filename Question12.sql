a744eff5dd8bee275301c7c73c949cb1bb3b182e
lucasfrcamargo <lucasfrcamargo@gmail.com>
2024-07-13 15:41:28 -0300

    Update Question12.sql

--- Question12.sql
+++ Question12.sql
@@ -1,31 +1,31 @@
 --Crie uma query que obtenha todos os dados de pedidos (Orders) que envolvam os fornecedores da cidade 'Manchester' e foram enviados pela empresa 'Speedy Express';
 
 
-SELECT O.[OrderID]					  AS [Order ID]      
+SELECT O.[OrderID]				  AS [Order ID]      
       ,O.[CustomerID]				  AS [Customer ID]
-	  ,C.CompanyName				  AS [Customer Name]
+      ,C.CompanyName				  AS [Customer Name]
       ,O.[EmployeeID]				  AS [Employee ID]
-	  ,E.FirstName + ' ' + E.LastName AS [Employee Name]
+      ,E.FirstName + ' ' + E.LastName 		  AS [Employee Name]
       ,O.[OrderDate]				  AS [Order Date]	
       ,O.[RequiredDate]				  AS [Required Date]	
       ,O.[ShippedDate]				  AS [Shipped Date]	
-      ,O.[ShipVia]					  AS [Ship Via]		
-	  ,SP.CompanyName				  AS [Shipping Company Name]
-      ,O.[Freight]					  AS [Freight]			
-      ,O.[ShipName]					  AS [Ship Name]			
+      ,O.[ShipVia]				  AS [Ship Via]		
+      ,SP.CompanyName				  AS [Shipping Company Name]
+      ,O.[Freight]				  AS [Freight]			
+      ,O.[ShipName]				  AS [Ship Name]			
       ,O.[ShipAddress]				  AS [Ship Address]		
-      ,O.[ShipCity]					  AS [Ship City]			
+      ,O.[ShipCity]				  AS [Ship City]			
       ,O.[ShipRegion]				  AS [Ship Region]		
       ,O.[ShipPostalCode]			  AS [Ship Postal Code]	
       ,O.[ShipCountry]				  AS [Ship Country]		
       ,P.[ProductID]				  AS [Product ID]		
-	  ,P.ProductName				  AS [Product Name]		
+      ,P.ProductName				  AS [Product Name]		
       ,OD.[UnitPrice]				  AS [Unit Price]	
       ,OD.[Quantity]				  AS [Quantity]	
       ,OD.[Discount]				  AS [Discount]	
-	  ,S.CompanyName				  AS [Supplies Company Name]	
-	  ,S.City						  AS [City]			
-  FROM Suppliers S
+      ,S.CompanyName				  AS [Supplies Company Name]	
+      ,S.City					  AS [City]			
+FROM Suppliers S
 INNER JOIN Products P ON P.SupplierID = S.SupplierID
 INNER JOIN [Order Details] OD ON OD.ProductID = P.ProductID
 INNER JOIN Orders O ON O.OrderID = OD.OrderID
