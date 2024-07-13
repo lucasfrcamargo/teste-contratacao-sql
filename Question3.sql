0e9f85d9513a0d19241386643bb116b0d284a4ab
lucasfrcamargo <lucasfrcamargo@gmail.com>
2024-07-13 15:42:38 -0300

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -1,7 +1,7 @@
 --Crie uma query que obtenha a lista de produtos descontinuados (ProductID e ProductName);
 
 SELECT ProductID
-	  ,ProductName
+      ,ProductName
 FROM Products
 WHERE Discontinued = 1
 
